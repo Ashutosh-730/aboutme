cd741da47c00c644cac494eea5d9ac0e7f50c7b5
Ashutosh-730 <ashutoshy730@gmail.com>
2023-05-04 20:37:08 +0530

    update resume

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <h1 className="display-3">Ashutosh Yadav</h1>
         <h3>An enthusiastic full-stack software developer</h3>
         <ul className="nav">
-          <li className="nav-item"><a className="nav-link" href="https://firebasestorage.googleapis.com/v0/b/aboutme-ashutosh.appspot.com/o/ashutosh_resume.pdf?alt=media&token=b719e932-6413-4c8c-aaf0-40c6278f7568" target="_blank" rel="noreferrer"><i className="fa fa-file-pdf-o"></i></a></li>
+          <li className="nav-item"><a className="nav-link" href="https://firebasestorage.googleapis.com/v0/b/aboutme-ashutosh.appspot.com/o/aboutme-ashutosh%2Fashutosh_resume.pdf?alt=media&token=c2983ea0-9ffa-4ede-82c7-01b55f447fad" target="_blank" rel="noreferrer"><i className="fa fa-file-pdf-o"></i></a></li>
           <li className="nav-item"><a className="nav-link" href="tel:+91-7990058793"><i className="fa fa-phone"></i></a></li>
           <li className="nav-item"><a className="nav-link" href="mailto:ashutoshy730@gmail.com"><i className="fa fa-envelope-o"></i></a></li>
           <li className="nav-item"><a className="nav-link" href="https://www.linkedin.com/in/ashutosh730" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i></a></li>
