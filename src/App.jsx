20d725fa0426713effd192c35cfc0092efaef02a
Ashutosh-730 <ashutoshy730@gmail.com>
2023-05-04 20:23:39 +0530

    pdf file path fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <h1 className="display-3">Ashutosh Yadav</h1>
         <h3>An enthusiastic full-stack software developer</h3>
         <ul className="nav">
-          <li className="nav-item"><a className="nav-link" href="./src/assets/ashutosh_resume.pdf" target="_blank" rel="noreferrer"><i className="fa fa-file-pdf-o"></i></a></li>
+          <li className="nav-item"><a className="nav-link" href="https://firebasestorage.googleapis.com/v0/b/aboutme-ashutosh.appspot.com/o/ashutosh_resume.pdf?alt=media&token=b719e932-6413-4c8c-aaf0-40c6278f7568" target="_blank" rel="noreferrer"><i className="fa fa-file-pdf-o"></i></a></li>
           <li className="nav-item"><a className="nav-link" href="tel:+91-7990058793"><i className="fa fa-phone"></i></a></li>
           <li className="nav-item"><a className="nav-link" href="mailto:ashutoshy730@gmail.com"><i className="fa fa-envelope-o"></i></a></li>
           <li className="nav-item"><a className="nav-link" href="https://www.linkedin.com/in/ashutosh730" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i></a></li>
